38af9cf6dc910190af4a71379cc95d00b7767838
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-04-03 21:27:59 +0530

    Update Youtube.py

--- ANIYAXMUSIC/platforms/Youtube.py
+++ ANIYAXMUSIC/platforms/Youtube.py
@@ -10,16 +10,16 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SIMMY_X_MUSIC.utils.database import is_on_off
-from SIMMY_X_MUSIC.utils.formatters import time_to_seconds
+from ANIYAXMUSIC.utils.database import is_on_off
+from ANIYAXMUSIC.utils.formatters import time_to_seconds
 
 # ─────────────────────────────
-# 🔥 EXTERNAL API CONFIG (Anti-Ban)
+# 🔥 SHRUTI API CONFIG
 # ─────────────────────────────
 FALLBACK_API_URL = "https://shrutibots.site"
 YOUR_API_URL = None
 
-cookies_file = "SIMMY_X_MUSIC/assets/cookies.txt"
+cookies_file = "ANIYAXMUSIC/assets/cookies.txt"
 
 # 🔄 API URL Loader (Dynamic)
 async def get_api_url():
@@ -48,7 +48,7 @@ async def download_via_shruti(link: str, is_video: bool = False):
     ext = "mp4" if is_video else "mp3"
     file_path = os.path.join(folder, f"{video_id}.{ext}")
 
-    # Cache Check (Agar pehle se downloaded hai toh wahi use karo)
+    # Cache Check
     if os.path.exists(file_path):
         return file_path
 
@@ -241,6 +241,7 @@ class YouTubeAPI:
             print(f"API Failed, trying Fallback: {e}")
 
         # 2️⃣ Fallback (YT-DLP) - Agar API fail ho jaye tabhi chalega
+        # Render par ye fail hi hoga, but safety ke liye rakha hai.
         loop = asyncio.get_running_loop()
 
         def audio_dl():
@@ -278,4 +279,4 @@ class YouTubeAPI:
             downloaded_file = await loop.run_in_executor(None, audio_dl)
             
         return downloaded_file, direct
-            
+                
